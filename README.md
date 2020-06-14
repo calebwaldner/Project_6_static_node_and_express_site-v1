48f190e55d2d0efd6d8d5ff8741cf14d4b3af4e6
Caleb Waldner <caleb.waldner@gmail.com>
2020-06-14 14:46:55 -0500

    Complete final project

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Project_6_static_node_and_express_site-v1
  Project 6 for the Treehouse Tech Degreee
+
+
+My additions to the project:
+- Removed "Learn More" link from the About page (since the link has no purpose when on the /about page).
+- Added video elements to a couple of the projects to provide a better preview of the application.
+- Change color theme. Changes include: 
+  - Background color for all pages
+  - Left menu and top bar (mobile) colors
+  - Link background color and link hover state color
+  - Boarder color for my thumbnail
+- Transition effect on project thumbnails on index page
+- Updated the responsive breakpoints and style to look better under 500px width.
---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const projectsData = require("./data.json");
+const app = express();
+
+app.set("view engine", "pug");
+app.use("/static", express.static("public"));
+
+// index page router
+app.get("/", (req, res) => {
+  res.render("index", {projectsData});
+});
+
+// about page router
+app.get("/about", (req, res) => {
+  res.render("about", {onAbout: true}); // onAbout is used to remove a redundant link from template
+});
+
+// project page router
+app.get(`/projects:id`, (req, res) => {
+  const id = req.params.id; // gets the id number that is sent in the request
+  const project = projectsData.projects[id] // gets the project clicked on and provides it to the template
+  res.render("project", {project});
+});
+
+// Error handling
+app.use((req, res, next) => {
+  // any requests that make it this far without being routed will produce this error;
+  const err = new Error("Sorry, page not found. Maybe this call stack will help?");
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.error = err; // this stores the error into the response object to be used anywhere the response is used.
+  console.log("There was an error...")
+  res.render("error");
+})
+
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
